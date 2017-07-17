9b0ee6fc71e96dbc496eb6f43727dd66296d7d63
萧瑟 <405192400@qq.com>
2017-07-17 18:06:09 +0800

    新增MenuController

---
+++ src/main/java/cn/test/web/controller/MenuController.java
@@ -0,0 +1,45 @@
+package cn.test.web.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.google.gson.Gson;
+
+import cn.test.common.po.DefaultRequest;
+import cn.test.common.po.PageHandler;
+import cn.test.common.po.Role;
+import cn.test.po.Menu;
+import cn.test.service.MenuService;
+import cn.test.service.RoleService;
+
+@RequestMapping("/menu")
+@Controller
+public class MenuController {
+	
+	@Autowired
+	private MenuService menuService;
+	
+	
+	
+	@RequestMapping(value="/list",method=RequestMethod.GET)
+	@ResponseBody
+	public Object list(PageHandler<Menu> page) {
+		DefaultRequest<Menu> d = menuService.getMenuList(page);
+		return d;
+	}
+	
+	
+	@RequestMapping(value="/add",method=RequestMethod.POST)
+	@ResponseBody
+	public Object add(Role role) {
+		DefaultRequest<Role> d = new DefaultRequest<>();
+
+		return d;
+	}
+	
+
+
+}
